46236f5b3e5742f4279f8be366406fc3149cd87c
GabrielVictor159 <gabrielvictor159487@gmail.com>
2023-01-21 06:47:10 -0300

    tentando adicionar cors na aplicação

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ from CalculosController import CalculosController
 app = Flask(__name__)
 CORS(app)
 calculos = CalculosController()
-
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
 @app.route("/pitagoras/<float:a>/<float:b>", methods=["GET"])
 def pitagoras_route(a, b):
     c = calculos.pitagoras(a, b)
